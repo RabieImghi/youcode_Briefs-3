c3b6b18260a4c0d91b885336a39cdf28c0899523
RabieImghi <rabieaitimghi7@gmail.com>
2023-10-16 17:09:41 +0200

    ajoute au panie en javascript wvec une button pour acces a detail panie

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let   cont = 0;
+let cont = 0;
+let countProduit=0;
 var tablImage=["images/women.png","images/women2.png","images/women3.png"];
 var image= document.getElementById("imageHomeSlide");
 function sliderImagesHomeNex(){
@@ -11,3 +12,22 @@ function sliderImagesHomePrev(){
     if(cont<0) cont = 2;
     image.src = tablImage[cont];
 }
+function panieCart(){
+    var cartActive = document.getElementById('cart-panie');
+    if(cartActive.style.display === "none") 
+    cartActive.style.display="block";
+    else cartActive.style.display="none";
+}
+function addPanie(){
+    var produitCount= document.getElementById("produitCount");
+    var cartActive = document.getElementById('produitsCard');
+    var prixProduit = document.getElementById('prixTotal');
+    produitCount.innerHTML = ++countProduit;
+    cartActive.innerHTML="";
+    for(var i=0;i<countProduit;i++){
+        cartActive.innerHTML+= "<div class='produits' style='display: flex; justify-content: space-between; margin:20px 10px;'> <h4>Produit Name</h4>  <h4>1900 $</h4> </div>";
+    }
+    prixProduit.innerHTML= countProduit*1900+" $";
+
+} 
+// definition of done planing pooker
