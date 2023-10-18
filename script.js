3bcf57c526b37dcf09b9458df7b5e79bb3cc918e
RabieImghi <rabieaitimghi7@gmail.com>
2023-10-18 11:59:08 +0200

    ajoute script afichage menu en mobile device avec workflow

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ var tablImage=["images/women.png","images/women2.png","images/women3.png"];
 var image= document.getElementById("imageHomeSlide");
 var previewsImage=document.getElementById("previews");
 var nextImage=document.getElementById("next");
+// Afichage l'image suivant
 nextImage.addEventListener('click',  function sliderImagesHomeNex(){
     if(cont<3) cont ++;
     if(cont==3) cont = 0;
     image.src = tablImage[cont];
 });
+// afichage l'image preview
 previewsImage.addEventListener('click', function sliderImagesHomePrev(){
     if(cont>=0) cont --;
     if(cont<0) cont = 2;
     image.src = tablImage[cont];
+});
+// afichge menu mobil
+var previewsImage=document.getElementById("afichagMenu");
+previewsImage.addEventListener('click', function afichageMenu(){
+    var menuMobile = document.getElementById('menuMobile');
+    if(menuMobile.className == "navigation_info") 
+        menuMobile.classList.add("menuBlock");
+    else if(menuMobile.className == "navigation_info menuBlock") 
+    menuMobile.classList.remove('menuBlock');
 });
 // definition of done planing pooker
